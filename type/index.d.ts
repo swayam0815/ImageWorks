824a0959508a8a916a229d12e5a71aa4d1be3934
Swayam <swayam0815@gmail.com>
2024-08-14 14:30:59 -0400

    another try

--- type/index.d.ts
+++ type/index.d.ts
@@ -4,7 +4,7 @@
 declare type CreateUserParams = {
     clerkId: string;
     email: string;
-    username: string;
+    username: string | null;
     firstName: string;
     lastName: string;
     photo: string;
